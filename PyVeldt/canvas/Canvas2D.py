3546c42df15536e2be7bd64a71c05908c2552a86
wh1ter0se <camelCaseEverything@gmail.com>
2022-06-28 23:37:26 -0500

    Added Canvas2D.

--- PyVeldt/canvas/Canvas2D.py
+++ PyVeldt/canvas/Canvas2D.py
@@ -5,6 +5,7 @@ class Canvas2D():
     def __init__(self, label:str) -> None:
         self.label = label
 
+        self.layers = []
         self.canvas_segments = []
         
 
@@ -15,31 +16,54 @@ class Canvas2D():
             self.start_pos = start_pos
             self.vector = vector
 
+
     class Layer():
+        def __init__(self): 
+            pass
+    
+
+    class FunctionLayer(Layer):
+
+        def __init__(self, init_func:function=(lambda x,y:0,0,0)):
+            ''''''
+            self.func = init_func
+
+        def set_func(self, func:function):
+            self.func = func
+
+
+    class ObjectLayer(Layer):
         def __init__(self):
-            self.objects = []
+            ''''''
+            pass
 
-    class Object():
-        def __init__(self, center:tuple, init_velocity:tuple=(0,0)):
-            self.center = center
-            self.velocity = init_velocity
+        class Object():
+            def __init__(self, center:tuple, init_velocity:tuple=(0,0)):
+                ''''''
+                self.center = center
+                self.velocity = init_velocity
 
-        def set_center(self, new_x, new_y):
-            self.center = (new_x, new_y)
-        
-        def get_RGBA(self, abs_position:tuple) -> tuple: raise NotImplementedError
+            def set_center(self, new_x, new_y):
+                ''''''
+                self.center = (new_x, new_y)
+            
+            def get_RGBA(self, abs_position:tuple) -> tuple: 
+                ''''''
+                raise NotImplementedError
 
-    class Circle(Object):
-        def __init__(self, center:tuple, radius:float, color, feather:float=0):
-            super().__init__(center)
-            self.radius = radius
-            self.color = color
-            self.feather = feather
+        class Circle(Object):
+            def __init__(self, center:tuple, radius:float, color, feather:float=0, init_velocity:tuple=(0,0)):
+                ''''''
+                super().__init__(center, init_velocity)
+                self.radius = radius
+                self.color = color
+                self.feather = feather
 
-        def get_RGBA(self, abs_position:tuple) -> tuple:
-            dist = distance(abs_position, self.center)
-            if dist <= self.radius: alpha = 1.0
-            elif dist < self.radius + self.feather:
-                alpha = 1.0 - ((dist - self.radius) / self.feather)
-            else: alpha = 0.0
-            return (self.color.RGB, alpha)
+            def get_RGBA(self, abs_position:tuple) -> tuple:
+                ''''''
+                dist = distance(abs_position, self.center)
+                if dist <= self.radius: alpha = 1.0 # inside circle
+                elif dist < self.radius + self.feather: # in feather region
+                    alpha = 1.0 - ((dist - self.radius) / self.feather)
+                else: alpha = 0.0 # outside circle
+                return (self.color.RGB, alpha)
