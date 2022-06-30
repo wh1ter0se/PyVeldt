65042fd2a35a840ca1e610ce100160488f216795
wh1ter0se <camelCaseEverything@gmail.com>
2022-06-29 21:15:45 -0500

    Map2D: The return of map!

--- PyVeldt/canvas/Canvas2D.py
+++ PyVeldt/canvas/Canvas2D.py
@@ -1,5 +1,17 @@
 from ..subsystems.LEDs.Strip import Segment
-from ..utils.math import distance
+from ..utils import math
+from typing import Tuple
+
+class Map2D():
+    def __init__(self) -> None:
+        self.segments = []
+
+    class MapSegment():
+        def __init__(self, segment:Segment, start_pos:Tuple(int,int), vector:Tuple(int,int)):
+            self.segment = segment
+            self.length = self.segment.length
+            self.start_pos = start_pos
+            self.vector = vector
 
 class Canvas2D():
     def __init__(self, label:str) -> None:
@@ -7,63 +19,59 @@ class Canvas2D():
 
         self.layers = []
         self.canvas_segments = []
-        
-
-    class CanvasSegment():
-        def __init__(self, segment:Segment, start_pos:tuple, vector:tuple):
-            self.segment = segment
-            self.length = self.segment.length
-            self.start_pos = start_pos
-            self.vector = vector
-
 
     class Layer():
-        def __init__(self): 
-            pass
-    
+        def __init__(self, label:str): 
+            self.label  = label
 
-    class FunctionLayer(Layer):
+    def add_layer(self, layer:Layer):
+        self.layers.append(layer)
 
-        def __init__(self, init_func:function=(lambda x,y:0,0,0)):
+
+class FunctionLayer(Canvas2D.Layer):
+
+    def __init__(self, label:str, init_func:function=(lambda x,y:0,0,0)):
+        ''''''
+        super().__init__(label)
+        self.func = init_func
+
+    def set_func(self, func:function):
+        self.func = func
+
+
+class ObjectLayer(Canvas2D.Layer):
+    def __init__(self, label:str):
+        ''''''
+        super().__init__(label)
+        pass
+
+    class Object():
+        def __init__(self, center:Tuple(int,int), init_velocity:Tuple(int,int)=(0,0)):
             ''''''
-            self.func = init_func
+            self.center = center
+            self.velocity = init_velocity
 
-        def set_func(self, func:function):
-            self.func = func
-
-
-    class ObjectLayer(Layer):
-        def __init__(self):
+        def set_center(self, new_x, new_y):
             ''''''
-            pass
+            self.center = (new_x, new_y)
+        
+        def get_RGBA(self, abs_position:Tuple(int,int)) -> Tuple(int,int,int,float): 
+            ''''''
+            raise NotImplementedError
 
-        class Object():
-            def __init__(self, center:tuple, init_velocity:tuple=(0,0)):
-                ''''''
-                self.center = center
-                self.velocity = init_velocity
+    class Circle(Object):
+        def __init__(self, center:Tuple(int,int), radius:float, color, feather:float=0, init_velocity:Tuple(int,int)=(0,0)):
+            ''''''
+            super().__init__(center, init_velocity)
+            self.radius = radius
+            self.color = color
+            self.feather = feather
 
-            def set_center(self, new_x, new_y):
-                ''''''
-                self.center = (new_x, new_y)
-            
-            def get_RGBA(self, abs_position:tuple) -> tuple: 
-                ''''''
-                raise NotImplementedError
-
-        class Circle(Object):
-            def __init__(self, center:tuple, radius:float, color, feather:float=0, init_velocity:tuple=(0,0)):
-                ''''''
-                super().__init__(center, init_velocity)
-                self.radius = radius
-                self.color = color
-                self.feather = feather
-
-            def get_RGBA(self, abs_position:tuple) -> tuple:
-                ''''''
-                dist = distance(abs_position, self.center)
-                if dist <= self.radius: alpha = 1.0 # inside circle
-                elif dist < self.radius + self.feather: # in feather region
-                    alpha = 1.0 - ((dist - self.radius) / self.feather)
-                else: alpha = 0.0 # outside circle
-                return (self.color.RGB, alpha)
+        def get_RGBA(self, abs_position:tuple) -> Tuple(int,int,int,float):
+            ''''''
+            dist = math.distance(abs_position, self.center)
+            if dist <= self.radius: alpha = 1.0 # inside circle
+            elif dist < self.radius + self.feather: # in feather region
+                alpha = 1.0 - ((dist - self.radius) / self.feather)
+            else: alpha = 0.0 # outside circle
+            return (self.color.RGB, alpha)
